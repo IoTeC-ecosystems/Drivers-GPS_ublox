cceda70abb05185d8380274521984e0f47150408
Jose Septien <jaseptienh@gmail.com>
2025-05-22 18:20:53 -0600

    Add more configuration commands.

--- gps_ublox.c
+++ gps_ublox.c
@@ -28,6 +28,17 @@ bool init_gps_ublox(uart_t _dev, uint32_t baud, uint16_t rate)
     _uart_dev = _dev;
     uart_init(_uart_dev, baud, gps_cb, NULL);
 
+    // Restore defaults
+    // CFG-CFG
+    uint8_t cfg_cfg[] = {
+        0xB5, 0x62, 0x06, 0x09, 0x0D, 0x00,
+        // Payload
+        0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x17,
+        // CK_A, CK_B
+        0x00, 0x00,
+    };
+    checksum(cfg_cfg, sizeof(cfg_cfg));
+    uart_write(_uart_dev, cfg_cfg, sizeof(cfg_cfg));
 
     // CFG-RATE - set update rate
     uint8_t cfg_rate[] = {
@@ -39,9 +50,6 @@ bool init_gps_ublox(uart_t _dev, uint32_t baud, uint16_t rate)
     };
     checksum(cfg_rate, sizeof(cfg_rate));
     uart_write(_uart_dev, cfg_rate, sizeof(cfg_rate));
-    for (uint8_t i = 0; i < sizeof(cfg_rate); i++) {
-        printf("%x ", cfg_rate[i]);
-    }
 
     // Nav engine settings
     uint8_t cfg_nav5[] = {
@@ -68,6 +76,27 @@ bool init_gps_ublox(uart_t _dev, uint32_t baud, uint16_t rate)
     };
     checksum(cfg_nav5, sizeof(cfg_nav5));
     uart_write(_uart_dev, cfg_nav5, sizeof(cfg_nav5));
+
+    // Disable nmea messages
+    uint8_t messages[][2] = {
+        {0xF0, 0x03},   // GSV
+        {0xF0, 0x02},   // GSA
+    };
+    uint8_t cfg_msg[] = {
+        0xB5, 0x62, 0x06, 0x01, 0x03, 0x00,
+        // payload
+        0x00, 0x00, 0x00,
+        // CK_A, CK_B
+        0x00, 0x00
+    };
+    uint8_t offset = 6;
+    for (uint8_t i = 0; i < sizeof(messages) / sizeof(*messages); i++) {
+        for (uint8_t j = 0; j < sizeof(*messages); j++) {
+            cfg_msg[offset + j] = messages[i][j];
+        }
+        checksum(cfg_msg, sizeof(cfg_msg));
+        uart_write(_uart_dev, cfg_msg, sizeof(cfg_msg));
+    }
 
     return true;
 }
